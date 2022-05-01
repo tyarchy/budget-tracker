380978d5350a8d3be067a71eef54b5b30b830f5d
tyarchy <ty.d.gregory@gmail.com>
2022-05-01 13:38:04 -0600

    yadda yadda

---
+++ public/js/indexdb.js
@@ -0,0 +1,82 @@
+let db;
+
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function (event) {
+    // save a reference to the database 
+    const db = event.target.result;
+
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+// upon a successful 
+request.onsuccess = function (event) {
+
+    db = event.target.result;
+
+    // check if app is online, if yes run uploadTransaction() function to send all local db data to api
+    if (navigator.onLine) {
+        uploadTransaction();
+    }
+};
+
+request.onerror = function (event) {
+    // log error here
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  
+    // access the object
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+  
+    // add record to your store with add method
+    budgetObjectStore.add(transaction);
+  };
+
+  function uploadTransaction() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+  
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+  
+    // upon a successful .getAll() execution, run this function
+getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_transaction'], 'readwrite');
+          // access the new_pizza object store
+          const budgetObjectStore = transaction.objectStore('new_transaction');
+          // clear all items in your store
+          budgetObjectStore.clear();
+
+          alert('Take no prisoners!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+  }
+
+  window.addEventListener('online', uploadTransaction);
