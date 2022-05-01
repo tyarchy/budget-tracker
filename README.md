5bf4bf5d05c941f1224ca57184d5d6684b147f82
tyarchy <ty.d.gregory@gmail.com>
2022-05-01 14:59:56 -0600

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+
+<h1> some html </h1>
+<button href="https://www.google.com">click me</button>
+[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
--- public/js/index.js
+++ public/js/index.js
@@ -136,7 +136,7 @@ function sendTransaction(isAdding) {
   })
   .catch(err => {
     // fetch failed, so save in indexed db
-    saveRecord(transaction);
+    saveRecord(record);
 
     // clear form
     nameEl.value = "";
